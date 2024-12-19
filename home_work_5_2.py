e3c5139c46f6896fc77bd32063cf025acf4437a4
Dmitry Miro <131314376+DmitryMlro@users.noreply.github.com>
2024-12-19 21:32:56 +0200

    Add files via upload

---
+++ home_work_5_2.py
@@ -0,0 +1,20 @@
+while True:
+    first_num = int(input("Введіть перше число: "))
+    math_operation = input("Вкажіть операцію (+, -, *, /): ")
+    second_num = int(input("Введіть друге число: "))
+
+    if math_operation == "+":
+        print("Відповідь:", first_num + second_num)
+    elif math_operation == "-":
+        print("Відповідь:", first_num - second_num)
+    elif math_operation == "*":
+        print("Відповідь:", first_num * second_num)
+    elif math_operation == "/" and second_num != 0:
+        print("Відповідь:", first_num / second_num)
+    else:
+        print("Дані введено некоректно, або дільник дорівнює 0")
+
+    turn_calc = input("Для продовження натсність - у: ").lower()
+    if turn_calc not in ("y", "yes"):
+        print("Роботу калькулятора завершено.")
+        break
